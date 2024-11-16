4c0b82037f209b8d04fca6ce309d2c6a4b9cdaac
Ivanov-coder <a3230269751@163.com>
2024-11-16 08:37:09 +0800

    updated

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ CACHE_PATH = "./cache/chat.json"
 logger = utils.logs.Logger.setup_logger(fileposition=__name__)
 
 
-
 # TODO: 在Qt中可能会存在开了Spark之后又开其它的情况，所以这里我们可能需要当窗口焦点改变时，做个挂起操作。
 async def run():
     # TODO: 这里的实例化需要做成选择框给用户选择模型
